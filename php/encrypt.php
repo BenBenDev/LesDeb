dfee32b36d52ef4074eb89b7f4022d42fd3361de
BenBenDev <benben.dev@gmail.com>
2018-05-30 17:36:44 +0200

    [site]  encrypted data (email + telephone + firstname + lastname) are added in the QRcode with AES-256 method

---
+++ php/encrypt.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once ('./password.php');
+
+function AESencrypt($data) {
+  $plaintext = $data;
+  $method = 'aes-256-cbc';
+  // $password is defined in password.php
+  // $password = '...';    
+
+  // Must be exact 32 chars (256 bit)
+  $password = substr(hash('sha256', $password, true), 0, 32);
+  //echo "Password:" . $password . "<br>";
+
+  // IV must be exact 16 chars (128 bit)
+  $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
+
+  // encryption
+  $encrypted = base64_encode(openssl_encrypt($plaintext, $method, $password, OPENSSL_RAW_DATA, $iv));
+  
+  // My secret message 
+  // $decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
+
+  // echo 'plaintext=' . $plaintext . "<br>";
+  // echo 'cipher=' . $method . "<br>";
+  // echo 'encrypted to: ' . $encrypted . "<br>";
+  // echo 'decrypted to: ' . $decrypted . "<br><br>";
+  
+  return $encrypted;
+  
+}
--- php/image.php
+++ php/image.php
@@ -1,30 +1,30 @@
 <?php
-echo ('image.php');
 
+function toQRcode($post_data, $suffix) {
+  //   https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
+  $url = 'https://api.qrserver.com/v1/create-qr-code/';
+  $data = array('ecc'=>'L', 'color'=>'FF0000', 'size' => '400x400', 'data' => $post_data);
 
-//   https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
-$url = 'https://api.qrserver.com/v1/create-qr-code/';
-$data = array('ecc'=>'M', 'color'=>'FF0000', 'size' => '300x300', 'data' => "j'ai des données dans ce QRcode ;) ");
+  // use key 'http' even if you send the request to https://...
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data)
+    )
+  );
+  $context  = stream_context_create($options);
+  // call to the API
+  $data = file_get_contents($url, false, $context);
 
-// use key 'http' even if you send the request to https://...
-$options = array(
-  'http' => array(
-    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-    'method'  => 'POST',
-    'content' => http_build_query($data)
-  )
-);
-$context  = stream_context_create($options);
-// call to the API
-$data = file_get_contents($url, false, $context);
-
-if ($data){
-  // string to GD resource
-  $image = imagecreatefromstring ($data);
-  //save resource to file
-  imagepng($image,'../QRcodes/qr.png');
-  //imagepng($image);
-} else {
-  echo('<p>Error : no image !</p>');
+  if ($data){
+    // string to GD resource
+    $image = imagecreatefromstring ($data);
+    //save resource to file
+    imagepng($image,'../QRcodes/qr-'. $suffix.'.png');
+    //imagepng($image);
+  } else {
+    echo('<p>Error : no image !</p>');
+  }
 }
 ?>
